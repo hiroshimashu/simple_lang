3799e0ab3db1fafb4c0ac39f9250e63cc0f6398c
hiroshimashu <hiroshimashu@gmail.com>
2020-02-15 12:43:33 +0900

    add: implement special method to print output

--- ch02/main.rb
+++ ch02/main.rb
@@ -1,13 +1,36 @@
 class Number < Struct.new(:value)
+    def to_s
+        value.to_s 
+    end 
+
+    def inspect 
+        "<<#{self}>>"
+    end
 end
 
 class Add < Struct.new(:left, :right)
+    def to_s
+        "#{left} + #{right}"
+    end 
+
+    def inspect
+        "<<#{self}>>"
+    end
 end
 
 class Multiply < Struct.new(:left, :right)
+    def to_s
+        "#{left} * #{right}"
+    end 
+
+    def inspect
+        "<<#{self}>>"
+    end
 end
 
 puts Add.new(
     Multiply.new(Number.new(1), Number.new(2)),
     Multiply.new(Number.new(3), Number.new(4))
 )
+
+puts Multiply.new(Number.new(1), Number.new(2))
